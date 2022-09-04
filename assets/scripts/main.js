989905b1b3ef267bed962d92cb786003ae543a38
jj0han <joaovieirabomfim@gmail.com>
2022-09-04 15:19:09 -0400

    ajustes de desing e adição de skills e projetos

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,11 @@
 //Escrever logo
-let i = 0;
-const logo = document.querySelector(".logo") 
+// let i = 0;
+// const logo = document.querySelector(".logo") 
 
-document.addEventListener("DOMContentLoaded", function typeW() {
-    nome = "<João Bomfim/>"
-    vel = Math.floor(Math.random()*150) + 50
-    setTimeout(typeW, vel)
-    logo.innerHTML += nome.charAt(i)
-    i++
-})
+// document.addEventListener("DOMContentLoaded", function typeW() {
+//     nome = "<João Bomfim/>"
+//     vel = Math.floor(Math.random()*150) + 50
+//     setTimeout(typeW, vel)
+//     logo.innerHTML += nome.charAt(i)
+//     i++
+// })
